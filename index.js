b05ee87294256056badf16ae773c5bde58820c7f
sahithikarnati <130591214+sahithikarnati@users.noreply.github.com>
2023-08-09 00:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,106 +1,55 @@
-function validateAge(td, dobobj) {
-    var age = td.getFullYear() - dobobj.getFullYear();
-    var m = td.getMonth() - dobobj.getMonth();
-    if (m < 0 || (m === 0 && td.getDate() < dobobj.getDate())) {
-      age--;
-    }
-    return age;
-  }
-  let dobelement = document.getElementById("dob");
-  dobelement.addEventListener("change", () => {
-    let [y,m,d] = document.getElementById("dob").value.split("-");
-    let dob = new Date(y,m,d);
-    let Today = new Date();
-    let age = validateAge(Today, dob);
-    if (age < 18 || age > 55) {
-      dobelement.setCustomValidity("age must lie in 18 and 55 years!!!");
-   
-      return;
-    } else {
-      dobelement.setCustomValidity("");
-    }
-  });
-  let form = document.getElementById("user-form");
-  
-  const retriveEntries = () => {
-    let entries = localStorage.getItem("userEntry");
-  
-    if (entries) {
-      entries = JSON.parse(entries);
-    } else {
-      entries = [];
-    }
-    return entries;
-  };
-  
-  let Entries = retriveEntries();
-  
-  const displayEntries = () => {
-    const entries = retriveEntries();
-  
-    const rows = entries
-      .map((entry) => {
-        const name = `<td class="td">${entry.name}</td>`;
-        const email = `<td class="td">${entry.email}</td>`;
-        const password = `<td class="td">${entry.password}</td>`;
-        const dob = `<td class="td">${entry.dob}</td>`;
-        const acceptTerms = `<td class="td">${entry.acceptTerms}</td>`;
-  
-        const row = `<tr>${name} ${email} ${password} ${dob} ${acceptTerms}</tr>`;
-        return row;
-      })
-      .join("\n");
-  
-    let tableDiv = document.getElementById("entrytbale");
-  
-    tableDiv.innerHTML = `<table>
-    <tr>
-      <th class="th">Name</th>
-      <th class="th">Email</th>
-      <th class="th">Password</th>
-      <th class="th">Dob</th>
-      <th class="th">Accepted terms?</th>
-    </tr>
-      ${rows}
-    </table>`;
-  };
-  
-  // const saveUserFrom = () => {
-  const saveUserFrom = (event) => {
-    event.preventDefault();
-  
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let dob = document.getElementById("dob").value;
-    let acceptTerms = document.getElementById("acceptTerms").checked;
-  
-    let entry_obj = {
-      name,
-      email,
-      password,
-      dob,
-      acceptTerms,
-    };
-  
-    Entries.push(entry_obj);
-  
-    localStorage.setItem("userEntry", JSON.stringify(Entries));
-  
-    displayEntries();
-  };
-  
-  form.addEventListener("submit", saveUserFrom);
-  
-  displayEntries();
-  const email = document.getElementById("email");
-  email.addEventListener("input", () => validate(email));
-  function validate(ele) {
-    if (ele.validity.typeMismatch) {
-      ele.setCustomValidity("The Email is not in the right format!!!");
-      ele.reportValidity();
-    } else {
-      ele.setCustomValidity("");
-    }
-  }
+document.addEventListener("DOMContentLoaded", () => {
+      const registrationForm = document.getElementById("registrationForm");
+      const savedData = document.getElementById("savedData");
+
+      // Load previously saved data from Local Storage
+      const savedEntries = JSON.parse(localStorage.getItem("savedEntries")) || [];
+
+      // Populate the table with saved data
+      savedEntries.forEach(entry => {
+        const newRow = savedData.insertRow();
+        const nameCell = newRow.insertCell(0);
+        const emailCell = newRow.insertCell(1);
+        const passwordCell= newRow.insertCell(2);
+        const dobCell = newRow.insertCell(3);
+        const acceptedTermsCell = newRow.insertCell(4);
+
+        nameCell.textContent = entry.name;
+        emailCell.textContent = entry.email;
+        passwordCell.textContent = entry.password;
+        dobCell.textContent = entry.dob;
+        acceptedTermsCell.textContent = entry.acceptedTerms ? "True" : "False";
+      });
+
+      registrationForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const dob = document.getElementById("dob").value;
+        const acceptedTerms = document.querySelector('input[name="terms"]').checked;
+
+        // Add new entry to savedEntries array
+        savedEntries.push({ name, email, password, dob, acceptedTerms });
         
+        // Save updated array to Local Storage
+        localStorage.setItem("savedEntries", JSON.stringify(savedEntries));
+
+        // Update the table
+        const newRow = savedData.insertRow();
+        const nameCell = newRow.insertCell(0);
+        const emailCell = newRow.insertCell(1);
+        const passwordCell= newRow.insertCell(2);
+        const dobCell = newRow.insertCell(3);
+        const acceptedTermsCell = newRow.insertCell(4);
+
+        nameCell.textContent = name;
+        emailCell.textContent = email;
+        passwordCell.textContent = password;
+        dobCell.textContent = dob;
+        acceptedTermsCell.textContent = acceptedTerms ? "True" : "False";
+
+        registrationForm.reset();
+      });
+    });
